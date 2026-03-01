f33e3dc9fd9a36084d7e04b71c1712aca063f695
Vatsal Dave <a1b2c3.12340@gmail.com>
2026-03-01 10:06:33 +0530

    Added Day5 - DOM manipulation and dark mode project

---
+++ Day5-HTML/script.js
@@ -0,0 +1,112 @@
+let para = document.querySelector(".text-content");
+let changeText = document.querySelector(".change-text-btn")
+let changeMode = document.querySelector(".toggle-btn")
+let btns = document.querySelector(".btn-container button")
+let textContent = document.querySelector(".text-content")
+let body = document.body
+let textFlag = true;
+let btnFlag = true
+let newPara = `She never liked cleaning the sink. It was beyond her comprehension how it got so dirty so quickly. It seemed that she was forced to clean it every other day. Even when she was extra careful to keep things clean and orderly, it still ended up looking like a mess in a couple of days. What she didn't know was there was a tiny creature living in it that didn't like things neat.I recollect that my first exploit in squirrel-shooting was in a grove of tall walnut-trees that shades one side of the valley. I had wandered into it at noontime, when all nature is peculiarly quiet, and was startled by the roar of my own gun, as it broke the Sabbath stillness around and was prolonged and reverberated by the angry echoes.It was their first date and she had been looking forward to it the entire week. She had her eyes on him for months, and it had taken a convoluted scheme with several friends to make it happen, but he'd finally taken the hint and asked her out. After all the time and effort she'd invested into it, she never thought that it would be anything but wonderful. It goes without saying that things didn't work out quite as she expected.Brenda never wanted to be famous. While most of her friends dreamed about being famous, she could see the negative aspects that those who wanted to be famous seemed to ignore. The fact that you could never do anything in public without being mobbed and the complete lack of privacy was something that she never wanted to experience. She also had no desire to have strangers speculating about every aspect of her life and what each thing she did was supposed to mean. Brenda was perfectly happy with her anonymous life where she could do exactly as she wanted without anyone else giving a damn. Thus, her overnight Internet celebrity was not something she was thrilled about as her friends told her how lucky she was.She reached her goal, exhausted. Even more chilling to her was that the euphoria that she thought she'd feel upon reaching it wasn't there. Something wasn't right. Was this the only feeling she'd have for over five years of hard work?They decided to find the end of the rainbow. While they hoped they would find a pot of gold, neither of them truly believed that the mythical pot would actually be there. Nor did they believe they could actually find the end of the rainbow. Still, it seemed like a fun activity for the day, and pictures of them chasing rainbows would look great on their Instagram accounts. They would have never believed they would actually find the end of a rainbow, and when they did, what they actually found there.He ordered his regular breakfast. Two eggs sunnyside up, hash browns, and two strips of bacon. He continued to look at the menu wondering if this would be the day he added something new. This was also part of the routine. A few seconds of hesitation to see if something else would be added to the order before demuring and saying that would be all. It was the same exact meal that he had ordered every day for the past two years.The bowl was filled with fruit. It seemed to be an overabundance of strawberries, but it also included blueberries, raspberries, grapes, and banana slices. This was the meal Sarah had every morning to start her day since she could remember. Why she decided to add chocolate as an option today was still a bit of a surprise, but she had been in the process of deciding she wanted to change her routine. This was a baby step to begin that start.Betty decided to write a short story and she was sure it was going to be amazing. She had already written it in her head and each time she thought about it she grinned from ear to ear knowing how wonderful it would be. She could imagine the accolades coming in and the praise she would receive for creating such a wonderful piece. She was therefore extremely frustrated when she actually sat down to write the short story and the story that was so beautiful inside her head refused to come out that way on paper.He sat across from her trying to imagine it was the first time. It wasn't. Had it been a hundred? It quite possibly could have been. Two hundred? Probably not. His mind wandered until he caught himself and again tried to imagine it was the first time.`
+
+let oldPara = `This biographical article related to French artistic gymnastics is a
+        stub. You can help Wikipedia by expanding it. InsideAR was the largest
+        Augmented Reality event in Europe. It was organized and supported by
+        metaio GmbH every year. The first event was held in 2010, had since
+        expanded globally and was run at multiple locations around the world.
+        However, after Apple purchased metaio in May 2015, metaio cancelled the
+        InsideAR conference 2015 without any statements about the conference's
+        future. The Fearing Mind is an American horror television series that
+        aired on the Fox Family Channel from October 21 until December 2, 2000.
+        Bill Fearing, a famous writer of suspense thrillers, gets his ideas from
+        things that happen in his family. When he gets an idea, the viewers
+        enter his mind and see the gruesome events unfold. It had a bronze
+        barrel and lacked a modern recoil system, using only an ineffective
+        spring-mounted spade brake, and was virtually obsolescent on its
+        introduction. Nonetheless, it was the standard field howitzer for the
+        Austrian Army at the outbreak of the war. Lithuanian Paralympic
+        Committee (Lithuanian: Lietuvos parolimpinis komitetas) was founded on
+        1990 and recognized by the International Paralympic Committee after one
+        year. A GUI may be designed for the requirements of a vertical market as
+        application-specific graphical user interfaces. Examples include
+        automated teller machines (ATM), point of sale (POS) touchscreens at
+        restaurants, self-service checkouts used in a retail store, airline
+        self-ticket and check-in, information kiosks in a public space, like a
+        train station or a museum, and monitors or control screens in an
+        embedded industrial application which employ a real-time operating
+        system (RTOS). Cell phones and handheld game systems also employ
+        application specific touchscreen GUIs. Newer automobiles use GUIs in
+        their navigation systems and multimedia centers, or navigation
+        multimedia center combinations. A GUI uses a combination of technologies
+        and devices to provide a platform that users can interact with, for the
+        tasks of gathering and producing information. A series of elements
+        conforming a visual language have evolved to represent information
+        stored in computers. This makes it easier for people with few computer
+        skills to work with and use computer software. The most common
+        combination of such elements in GUIs is the windows, icons, menus,
+        pointer (WIMP) paradigm, especially in personal computers. In personal
+        computers, all these elements are modeled through a desktop metaphor to
+        produce a simulation called a desktop environment in which the display
+        represents a desktop, on which documents and folders of documents can be
+        placed. Window managers and other software combine to simulate the
+        desktop environment with varying degrees of realism. Entries may appear
+        in a list to make space for text and details, or in a grid for
+        compactness and larger icons with little space underneath for text.
+        Variations inbetween exist, such as a list with multiple columns of
+        items and a grid of items with rows of text extending sideways from the
+        icon. Multi-row and multi-column layouts commonly found on the web are
+        "shelf" and "waterfall". The former is found on image search engines,
+        where images appear with a fixed height but variable length, and is
+        typically implemented with the CSS property and parameter display:
+        inline-block;. A waterfall layout found on Imgur and Tweetdeck with
+        fixed width but variable height per item is usually implemented by
+        specifying column-width:. Smaller app mobile devices such as personal
+        digital assistants (PDAs) and smartphones typically use the WIMP
+        elements with different unifying metaphors, due to constraints in space
+        and available input devices. Applications for which WIMP is not well
+        suited may use newer interaction techniques, collectively termed
+        post-WIMP user interfaces. As of 2011, some touchscreen-based operating
+        systems such as Apple's iOS (iPhone) and Android use the class of GUIs
+        named post-WIMP. These support styles of interaction using more than one
+        finger in contact with a display, which allows actions such as pinching
+        and rotating, which are unsupported by one pointer and mouse. Human
+        interface devices, for the efficient interaction with a GUI include a
+        computer keyboard, especially used together with keyboard shortcuts,
+        pointing devices for the cursor (or rather pointer) control: mouse,
+        pointing stick, touchpad, trackball, joystick, virtual keyboards, and
+        head-up displays (translucent information devices at the eye level).
+        There are also actions performed by programs that affect the GUI. For
+        example, there are components like inotify or D-Bus to facilitate
+        communication between computer programs. GUIs were a hot topic in the
+        early 1980s. The Apple Lisa was released in 1983, and various windowing
+        systems existed for DOS operating systems (including PC GEM and
+        PC/GEOS).`
+
+changeText.addEventListener("click", function() {
+    para.textContent = textFlag ? newPara : oldPara;
+    textFlag = !textFlag;
+});
+
+changeMode.addEventListener("click",function () {
+    if(btnFlag == true) {
+        body.style.backgroundColor = "black"
+        textContent.style.backgroundColor = "#F0F7EE"
+        textContent.style.color = "black"
+        textContent.style.borderColor = "red"
+        changeMode.style.backgroundColor = "white"
+        changeMode.style.color = "#04395f"
+        changeText.style.backgroundColor = "white"
+        changeText.style.color = "#04395f"
+        btnFlag = false
+    }
+    else {
+        body.style.backgroundColor = "white"
+        textContent.style.backgroundColor = "#776871"
+        textContent.style.color = "white"
+        textContent.style.borderColor = "black"
+        changeMode.style.backgroundColor = "#04395f"
+        changeMode.style.color = "white"
+        changeText.style.backgroundColor = "#04395f"
+        changeText.style.color = "white"
+        btnFlag = true
+    }
+})
